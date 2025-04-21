d99f8c79a23fa7b3e2d57c16e566a95fb916472b
paranoiachains <bluntsside@gmail.com>
2025-04-22 00:36:11 +0300

    CreateUser method

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,12 +1,20 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/paranoiachains/loyalty-api/internal/auth"
 	"github.com/paranoiachains/loyalty-api/internal/logger"
+	"github.com/paranoiachains/loyalty-api/internal/models"
 	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var ErrUniqueUsername = errors.New("username already exists")
 
 // our database variable we will use throughout the process
 var DB Storage
@@ -14,6 +22,7 @@ var DB Storage
 type Storage interface {
 	Update() error
 	Return() error
+	CreateUser(ctx context.Context, creds auth.Credentials) (*models.User, error)
 }
 
 type PostgresStorage struct {
@@ -29,7 +38,7 @@ func (db PostgresStorage) Return() error {
 }
 
 func Connect(databaseURI string) error {
-	logger.Log.Info("connection to db", zap.String("database uri", databaseURI))
+	logger.Log.Info("connecting to db...")
 	db, err := sql.Open("pgx", databaseURI)
 	if err != nil {
 		logger.Log.Error("init db connection error", zap.Error(err))
@@ -42,4 +51,50 @@ func Connect(databaseURI string) error {
 	logger.Log.Info("successufully connected to db")
 	DB = PostgresStorage{db}
 	return nil
+}
+
+func (db PostgresStorage) CreateUser(ctx context.Context, creds auth.Credentials) (*models.User, error) {
+	logger.Log.Info("creating user...")
+	query := `
+	INSERT INTO users (username, password, balance, withdrawn)
+	VALUES ($1, $2, $3, $4)`
+
+	// bcrypt encryption of password
+	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 12)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = tx.Rollback()
+	}()
+
+	// creating user
+	_, err = tx.ExecContext(ctx, query, creds.Username, encryptedPassword, 0, 0)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		// if username already taken
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return nil, ErrUniqueUsername
+		}
+		return nil, err
+	}
+
+	// also return user model
+	var user models.User
+	row := tx.QueryRowContext(ctx, "SELECT * FROM users WHERE username=$1", creds.Username)
+	err = row.Scan(&user.UserID, &user.Username, &user.Password, &user.Balance, &user.Withdrawn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+	logger.Log.Info("user successfully created!")
+	return &user, nil
 }
