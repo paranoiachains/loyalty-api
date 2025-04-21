ab8f4ba9312721215d5126601ee64eff754d6903
paranoiachains <bluntsside@gmail.com>
2025-04-21 14:21:37 +0300

    initial structure

---
+++ cmd/gophermart/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/paranoiachains/loyalty-api/internal/handlers"
+	"github.com/paranoiachains/loyalty-api/internal/middleware"
+)
+
+func main() {
+	router := gin.New()
+	router.Use(gin.Recovery(), middleware.Logger(), middleware.Compression())
+
+	group := router.Group("/api/user/")
+	group.POST("register", handlers.Register)
+	group.POST("login", handlers.Login)
+	group.POST("orders", handlers.LoadOrder)
+	group.GET("orders", handlers.GetOrder)
+	group.GET("balance", handlers.GetBalance)
+	group.POST("balance/withdraw", handlers.RequestWithdraw)
+	group.GET("withdrawals", handlers.Withdrawals)
+
+	router.Run()
+}
---
+++ internal/handlers/orders.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func LoadOrder(c *gin.Context) {
+
+}
+
+func GetOrder(c *gin.Context) {
+
+}
