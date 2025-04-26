898eab83013c7556664aa08c16bc23e008c2ce96
paranoiachains <bluntsside@gmail.com>
2025-04-26 13:27:42 +0300

    incapsulation, App struct passed to handlers

--- order-service/cmd/gophermart/main.go
+++ order-service/cmd/gophermart/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
 	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/paranoiachains/loyalty-api/order-service/internal/auth"
-	"github.com/paranoiachains/loyalty-api/order-service/internal/database"
 	"github.com/paranoiachains/loyalty-api/order-service/internal/handlers"
+	"github.com/paranoiachains/loyalty-api/pkg/app"
+	"github.com/paranoiachains/loyalty-api/pkg/database"
 	"github.com/paranoiachains/loyalty-api/pkg/flags"
 	"github.com/paranoiachains/loyalty-api/pkg/logger"
 	"github.com/paranoiachains/loyalty-api/pkg/messaging"
@@ -20,28 +20,33 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Recovery(), middleware.Logger(), middleware.Compression())
 
-	// connect to db only once
-	var once sync.Once
+	var (
+		once     sync.Once
+		orderApp *app.App
+	)
+
 	once.Do(func() {
 		logger.Log.Debug("DB connection", zap.String("DSN", flags.DatabaseDSN))
-		err := database.ConnectToPostgres(flags.DatabaseDSN)
+		db, err := database.Connect(flags.DatabaseDSN)
 		if err != nil {
 			panic(err)
 		}
+
+		orderKafka := messaging.InitOrderKafka()
+
+		orderApp = &app.App{
+			DB:    db,
+			Kafka: orderKafka,
+		}
 	})
 
-	// start kafka services
-	messaging.OrderKafka = messaging.InitOrderKafka()
-	messaging.OrderKafka.Start(context.Background())
-	messaging.OrderKafka.Send([]byte{1, 0, 1, 0})
-
-	r.POST("/api/user/register", handlers.Register)
-	r.POST("/api/user/login", handlers.Login)
+	r.POST("/api/user/register", handlers.Register(orderApp))
+	r.POST("/api/user/login", handlers.Login(orderApp))
 
 	authGroup := r.Group("/")
 	authGroup.Use(auth.Auth())
 	{
-		authGroup.POST("/api/user/orders", handlers.LoadOrder)
+		authGroup.POST("/api/user/orders", handlers.LoadOrder(orderApp))
 		authGroup.GET("/api/user/orders", handlers.GetOrder)
 		authGroup.GET("/api/user/balance", handlers.GetBalance)
 		authGroup.POST("/api/user/balance/withdraw", handlers.RequestWithdraw)
