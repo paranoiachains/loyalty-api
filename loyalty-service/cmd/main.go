2eae0353bf1d52a1af3a428692ca4f459022fbd2
paranoiachains <bluntsside@gmail.com>
2025-04-26 13:29:21 +0300

    App struct, process goroutine

--- loyalty-service/cmd/main.go
+++ loyalty-service/cmd/main.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/paranoiachains/loyalty-api/loyalty-service/internal/process"
+	"github.com/paranoiachains/loyalty-api/pkg/app"
+	"github.com/paranoiachains/loyalty-api/pkg/database"
 	"github.com/paranoiachains/loyalty-api/pkg/flags"
+	"github.com/paranoiachains/loyalty-api/pkg/logger"
 	"github.com/paranoiachains/loyalty-api/pkg/messaging"
 )
 
 func main() {
-	// init kafka services
-	messaging.LoyaltyKafka = messaging.InitLoyaltyKafka()
-	messaging.LoyaltyKafka.Start(context.Background())
-	for v := range messaging.LoyaltyKafka.Receive() {
-		fmt.Printf("%v\n", v)
-	}
+	var (
+		once       sync.Once
+		loyaltyApp app.App
+	)
+
+	once.Do(func() {
+		logger.Log.Info("connecting to db...")
+		db, err := database.Connect(flags.DatabaseDSN)
+		if err != nil {
+			panic(err)
+		}
+		logger.Log.Info("successfully connected to db!")
+
+		loyaltyKafka := messaging.InitLoyaltyKafka()
+
+		loyaltyApp = app.App{
+			DB:    db,
+			Kafka: loyaltyKafka,
+		}
+	})
+
+	go process.Process(context.Background(), &loyaltyApp)
 
 	r := gin.New()
 	r.Run(flags.AccrualSystemAddress)
