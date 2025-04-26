586f20a2425f721753e43733e2d50cf178c611a2
paranoiachains <bluntsside@gmail.com>
2025-04-26 13:28:43 +0300

    gin.Handler func return, pass App struct

--- order-service/internal/handlers/login.go
+++ order-service/internal/handlers/login.go
@@ -5,33 +5,36 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/paranoiachains/loyalty-api/order-service/internal/auth"
+	"github.com/paranoiachains/loyalty-api/pkg/app"
 	"github.com/paranoiachains/loyalty-api/pkg/logger"
 	"go.uber.org/zap"
 )
 
-func Login(c *gin.Context) {
-	var creds auth.Credentials
-	if err := c.ShouldBindJSON(&creds); err != nil {
-		logger.Log.Error("json request", zap.Error(err))
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
+func Login(app *app.App) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var creds auth.Credentials
+		if err := c.ShouldBindJSON(&creds); err != nil {
+			logger.Log.Error("json request", zap.Error(err))
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+		user, err := creds.Authenticate(c.Request.Context(), app.DB)
+		if user == nil && err == nil {
+			logger.Log.Info("validation error, couldn't authenticate")
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		if err != nil {
+			logger.Log.Error("authenticate", zap.Error(err))
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		err = auth.SetCookies(c, user.UserID)
+		if err != nil {
+			logger.Log.Error("setting cookies", zap.Error(err))
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		c.String(http.StatusOK, "logged in")
 	}
-	user, err := creds.Authenticate()
-	if user == nil && err == nil {
-		logger.Log.Info("validation error, couldn't authenticate")
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-	if err != nil {
-		logger.Log.Error("authenticate", zap.Error(err))
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-	err = auth.SetCookies(c, user.UserID)
-	if err != nil {
-		logger.Log.Error("setting cookies", zap.Error(err))
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-	c.String(http.StatusOK, "logged in")
 }
--- order-service/internal/handlers/registration.go
+++ order-service/internal/handlers/registration.go
@@ -7,38 +7,41 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/paranoiachains/loyalty-api/order-service/internal/auth"
-	"github.com/paranoiachains/loyalty-api/order-service/internal/database"
+	"github.com/paranoiachains/loyalty-api/pkg/app"
+	"github.com/paranoiachains/loyalty-api/pkg/database"
 	"github.com/paranoiachains/loyalty-api/pkg/logger"
 	"go.uber.org/zap"
 )
 
-func Register(c *gin.Context) {
-	var creds auth.Credentials
-	if err := c.ShouldBindJSON(&creds); err != nil {
-		logger.Log.Error("json request", zap.Error(err))
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
-	}
-
-	user, err := database.DB.CreateUser(context.Background(), creds.Password, creds.Username)
-	if err != nil {
-		logger.Log.Error("creating user", zap.Error(err))
-
-		if errors.Is(err, database.ErrUniqueUsername) {
-			c.AbortWithStatus(http.StatusConflict)
+func Register(app *app.App) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var creds auth.Credentials
+		if err := c.ShouldBindJSON(&creds); err != nil {
+			logger.Log.Error("json request", zap.Error(err))
+			c.AbortWithStatus(http.StatusBadRequest)
 			return
 		}
 
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
+		user, err := app.DB.CreateUser(context.Background(), creds.Password, creds.Username)
+		if err != nil {
+			logger.Log.Error("creating user", zap.Error(err))
 
-	err = auth.SetCookies(c, user.UserID)
-	if err != nil {
-		logger.Log.Error("setting cookies", zap.Error(err))
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
+			if errors.Is(err, database.ErrUniqueUsername) {
+				c.AbortWithStatus(http.StatusConflict)
+				return
+			}
 
-	c.String(http.StatusOK, "user created")
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		err = auth.SetCookies(c, user.UserID)
+		if err != nil {
+			logger.Log.Error("setting cookies", zap.Error(err))
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		c.String(http.StatusOK, "user created")
+	}
 }
