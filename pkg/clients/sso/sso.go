7c3ef4d5a25abe0ddaea09f97faa2bdc8bc80084
paranoiachains <bluntsside@gmail.com>
2025-04-28 15:25:18 +0300

    error handling from grpc

--- pkg/clients/sso/sso.go
+++ pkg/clients/sso/sso.go
@@ -2,10 +2,19 @@ package sso
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	sso_grpc "github.com/paranoiachains/loyalty-api/grpc-service/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrWrongPassword     = errors.New("wrong password")
+	ErrUserAlreadyExists = errors.New("such user already exists")
 )
 
 type Client struct {
@@ -29,7 +38,17 @@ func (c *Client) Login(ctx context.Context, login string, password string) (stri
 		Password: password,
 	})
 	if err != nil {
-		return "", err
+		st, ok := status.FromError(err)
+		if ok {
+			switch st.Code() {
+			case codes.PermissionDenied:
+				return "", ErrWrongPassword
+			default:
+				return "", fmt.Errorf("unexpected grpc error: %w", err)
+			}
+		} else {
+			return "", err
+		}
 	}
 
 	return resp.Token, nil
@@ -41,7 +60,17 @@ func (c *Client) RegisterNewUser(ctx context.Context, login string, password str
 		Password: password,
 	})
 	if err != nil {
-		return 0, err
+		st, ok := status.FromError(err)
+		if ok {
+			switch st.Code() {
+			case codes.AlreadyExists:
+				return 0, ErrUserAlreadyExists
+			default:
+				return 0, fmt.Errorf("unexpected grpc error: %w", err)
+			}
+		} else {
+			return 0, err
+		}
 	}
 
 	return resp.UserId, nil
