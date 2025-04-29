6b1d9a38e045751b796f4d34dfcc92ed120a49b3
paranoiachains <bluntsside@gmail.com>
2025-04-29 21:49:21 +0300

    remove .ps1 script

--- rebuid_services.ps1
+++
@@ -1,3 +0,0 @@
-docker compose -f ./deploy/docker-compose.yml stop order-service loyalty-service sso-service
-docker compose -f ./deploy/docker-compose.yml rm -f order-service loyalty-service sso-service
-docker compose -f ./deploy/docker-compose.yml up --build -d order-service loyalty-service sso-service
